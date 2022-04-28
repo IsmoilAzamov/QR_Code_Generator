3facf0f75013f12eadade8f04b09f0d8a199ac62
ismoil <githubsdl@gmail.com>
2022-04-28 22:35:28 +0500

    added image saver

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,14 @@
+import 'dart:typed_data';
+
 import 'package:dictionary/style/style.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+
+import 'package:screenshot/screenshot.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+
+
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,6 +19,9 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   String data = "";
+
+   ScreenshotController screenshotController=ScreenshotController();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -20,34 +31,87 @@ class _HomeScreenState extends State<HomeScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Center(
-            child: QrImage(
-              data: data,
-              backgroundColor: Colors.white,
-              size: 300,
-            ),
+           Screenshot(
+             controller: screenshotController,
+             child: Center(
+              child: QrImage(
+
+                data: data,
+                backgroundColor: Colors.white,
+                size: 300,
+                version: QrVersions.auto,
+
+              ),
           ),
+
+           ),
           const SizedBox(
             height: 24,
           ),
-          Container(
+
+          SizedBox(
             width: 300,
-            child: TextField(
-              decoration: InputDecoration(
+            child:  TextField(
+              onChanged: (value){
+                setState(() {
+                  data=value;
+                });
+              },
+              textAlign: TextAlign.center,
+              decoration: const InputDecoration(
+
                   hintText: "Enter Your Link",
                   focusColor: Colors.white,
                   hoverColor: Colors.white,
                   filled: true,
                   fillColor: Colors.white),
-              style: TextStyle(
+              style: const  TextStyle(
                 color: Colors.blue,
                 fontSize: 21,
                 fontWeight: FontWeight.w400,
               ),
             ),
-          )
+          ),
+          const SizedBox(height: 24,),
+          RawMaterialButton(onPressed: (){
+            screenshotController
+                .capture(delay: const Duration(milliseconds: 10))
+                .then((capturedImage) async {
+              showCapturedWidget(context, capturedImage!);
+            }).catchError((onError) {
+
+            });
+
+          },
+          fillColor: AppStyle.accentColor,
+          shape:  const StadiumBorder(),
+          padding: const EdgeInsets.symmetric(horizontal: 66, vertical: 26),
+          child: const Text("Share QR Code", style: TextStyle(
+            fontSize: 24,
+            color: Colors.white, fontWeight: FontWeight.bold,
+          ),),)
         ],
       ),
     ));
+
   }
+  Future<dynamic> showCapturedWidget(
+      BuildContext context, Uint8List capturedImage) async{
+    return _saved(capturedImage);
+  }
+
+
+
+  _saved(Uint8List image) async {
+    final result = await ImageGallerySaver.saveImage(image);
+
+  }
+
+
 }
+
+
+
+
+
+
